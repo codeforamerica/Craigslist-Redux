de3b6c031589b274ad47f5f32b45dcb8483abfd1
Pete Fecteau <pete@buttonpresser.com>
2011-09-23 16:41:29 -0700

    changed links in stylesheets

---
+++ bookmarklet.js
@@ -0,0 +1,13 @@
+if (typeof jQuery == 'undefined') {
+	var jQ = document.createElement('script');
+	jQ.type = 'text/javascript';
+	jQ.onload=runthis;
+	jQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+	document.body.appendChild(jQ);
+} else {
+	runthis();
+}
+
+function runthis() {
+	// your JavaScript code goes here!
+}
