6ce9a1d293af01cae36ff45c796bc46ad13f120e
Pete Fecteau <pete@buttonpresser.com>
2011-12-13 13:52:11 -0800

    adding what ever changes I've made. Lost track

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,13 +1,11 @@
-if (typeof jQuery == 'undefined') {
-	var jQ = document.createElement('script');
-	jQ.type = 'text/javascript';
-	jQ.onload=runthis;
-	jQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-	document.body.appendChild(jQ);
-} else {
-	runthis();
+javascript:(function(){
+	if (!document.getElementById('awesomestylesheet')){
+		var objHead = document.getElementsByTagName('head'); 
+		if (objHead[0]){if (document.createElementNS && objHead[0].tagName == 'head') var objCSS = objHead[0].appendChild(document.createElementNS('http://www.w3.org/1999/xhtml', 'link')); 
+		else var objCSS = objHead[0].appendChild(document.createElement('link')); 
+		objCSS.id = 'awesomestylesheet'; objCSS.rel = 'stylesheet'; 
+		objCSS.href = 'http://buttonpresser.com/CLRDX/styles/countries.css'; 
+		objCSS.type = 'text/css';
+	}
 }
-
-function runthis() {
-	// your JavaScript code goes here!
-}
+})()
